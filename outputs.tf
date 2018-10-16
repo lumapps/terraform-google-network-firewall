8f3c5e721a200f7fa79cc4c6d273b15165f96cb3
Amaury Gauthier <amaury@lumapps.com>
2018-10-16 16:09:15 +0200

    Add output of the targer tag

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "self_link" {
   description = "The URI of the created resource"
   value       = "${google_compute_firewall.new-firewall.self_link}"
 }
+
+output "tag" {
+  description = "The target tag"
+  value       = "${google_compute_firewall.new-firewall.target_tags[0]}"
+}
